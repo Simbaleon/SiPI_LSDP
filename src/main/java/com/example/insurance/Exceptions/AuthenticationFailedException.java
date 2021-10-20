b1f184f6455d963f8abe6f850cf4ba849770d70b
Alexander Kanenkov <al.kanenckoff@yandex.ru>
2021-10-20 22:26:25 +0300

    initialized custom AuthenticationFilter and exception for authentication failure and fixed return statement in attemptAuthentication method

---
+++ src/main/java/com/example/insurance/Exceptions/AuthenticationFailedException.java
@@ -0,0 +1,11 @@
+package com.example.insurance.Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class AuthenticationFailedException extends ResponseStatusException {
+
+    public AuthenticationFailedException() {
+        super(HttpStatus.UNAUTHORIZED, "Введены неверные логин/пароль");
+    }
+}
---
+++ src/main/java/com/example/insurance/Security/Filters/JWTAuthenticationFilter.java
@@ -0,0 +1,48 @@
+package com.example.insurance.Security.Filters;
+
+import com.example.insurance.Data.Entities.UserEntity;
+import com.example.insurance.Exceptions.AuthenticationFailedException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@AllArgsConstructor
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+//        return super.attemptAuthentication(request, response);
+        try {
+            UserEntity user = new ObjectMapper().readValue(request.getInputStream(), UserEntity.class);
+            return authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword(), user.getRoles()
+                    )
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+//        super.unsuccessfulAuthentication(request, response, failed);
+        throw new AuthenticationFailedException();
+    }
+}
