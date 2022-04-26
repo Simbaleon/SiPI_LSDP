28750a5c53f49488d1acc75d6657a69735036bea
Alexander Kanenkov <al.kanenckoff@yandex.ru>
2022-04-27 01:59:24 +0300

    изменил хедер, требуется доработка по вёрстке

--- src/main/webapp/lsdp-frontend/src/components/common/content/Content.js
+++ src/main/webapp/lsdp-frontend/src/components/common/content/Content.js
@@ -1,12 +1,14 @@
 import s from "./Content.module.css";
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes,} from "react-router-dom";
 import AllOrders from "../../orders/Orders";
-import PersonalAccount from "../../personalAccount/PersonalAccount";
+import PersonalAccount from "../../user/personalAccount/PersonalAccount";
+import LoginPage from "../../user/loginPage/LoginPage";
 
 function Content() {
     return (
         <div className={s.content}>
             <Routes>
+                <Route path="/signin" element={<LoginPage/>}/>
                 <Route path="/orders" element={<AllOrders/>}/>
                 <Route path="/personalAccount" element={<PersonalAccount/>}/>
             </Routes>
--- src/main/webapp/lsdp-frontend/src/components/common/header/Header.js
+++ src/main/webapp/lsdp-frontend/src/components/common/header/Header.js
@@ -1,15 +1,82 @@
-import s from "./Header.module.css"
+import {AppBar, Button, Toolbar, Typography} from "@mui/material";
+import {makeStyles} from "@mui/styles";
 import {NavLink} from "react-router-dom";
 
+const useStyles = makeStyles(() => ({
+    header: {
+        backgroundColor: "#1c1427",
+        paddingRight: "79px",
+        paddingLeft: "118px",
+    },
+    logo: {
+        fontFamily: "Suez One, serif",
+        fontWeight: 600,
+        color: "white",
+        textAlign: "left",
+    },
+    menuButton: {
+        fontFamily: "Open Sans, sans-serif",
+        fontWeight: 700,
+        size: "18px",
+        marginLeft: "38px",
+    },
+    toolbar: {
+        display: "flex",
+        justifyContent: "space-between",
+    }
+}));
+
+const headersData = [
+    {
+        label: "Войти",
+        href: "/signin",
+    },
+    {
+        label: "Зарегистрироваться",
+        href: "/signup",
+    }
+];
+
+const getMenuButtons = (menuButton) => {
+
+    return headersData.map(({ label, href }) => {
+        return (
+            <Button
+                {...{
+                    key: label,
+                    color: "inherit",
+                    to: href,
+                    component: NavLink,
+                    className: menuButton
+                }}
+            >
+                {label}
+            </Button>
+        );
+    });
+};
+
 function Header() {
+    const { header, logo, menuButton, toolbar } = useStyles()
+
     return (
-        <div className={s.header}>
-            <div className={s.logo}>Freelance-platform</div>
-            <div className={s.menu}>
-                <NavLink to="/personalAccount">Личный кабинет</NavLink>
-                <NavLink to="/orders">Доступные заказы</NavLink>
-            </div>
-        </div>
+        // <div className={s.header}>
+        //     <div className={s.logo}>Freelance-platform</div>
+        //     <div className={s.menu}>
+        //         <NavLink to="/personalAccount">Личный кабинет</NavLink>
+        //         <NavLink to="/orders">Доступные заказы</NavLink>
+        //     </div>
+        // </div>
+        <header>
+            <AppBar position={"static"} className={header}>
+                <Toolbar className={toolbar}>
+                    <Typography variant="h6" component="h1" className={logo}>
+                        Freelance-platform
+                    </Typography>
+                    <div>{getMenuButtons(menuButton)}</div>
+                </Toolbar>
+            </AppBar>
+        </header>
     )
 }
 
