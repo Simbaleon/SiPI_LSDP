2be86271870943b59caf6f918a0a4b1fa341f969
Alexander Kanenkov <al.kanenckoff@yandex.ru>
2022-05-01 14:46:34 +0300

    очистка local storage при закрытии вкладки браузера

--- src/main/webapp/lsdp-frontend/src/App.js
+++ src/main/webapp/lsdp-frontend/src/App.js
@@ -2,8 +2,14 @@ import './App.css';
 import Header from "./components/common/header/Header";
 import Content from "./components/common/content/Content";
 import Footer from "./components/common/footer/Footer";
+import {useContext} from "react";
+import {Context} from "./index";
 
 function App() {
+    const {userStore} = useContext(Context)
+    window.addEventListener("beforeunload", function(e){
+        userStore.logout()
+    }, false);
     return (
         <div className="App">
             <Header/>
